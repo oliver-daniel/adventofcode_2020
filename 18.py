688733f948cab8bc7d99e968e6c0b30bbe87024a
Oliver Daniel <oliverdaniel00@gmail.com>
2020-12-18 14:22:19 -0500

    maximum day 18 curse

--- 18.py
+++ 18.py
@@ -3,10 +3,8 @@ N = ["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]
 
 N = list(map(str.strip, open('./in/18.txt').readlines()))
 
-SYMBOLS = {
-                '+': '__add__',
-                '*': '__mul__'
-            }
+SYMBOLS = {'+': '__add__', '*': '__mul__'}
+
 
 def outer_brackets(s):
     ret = []
@@ -14,14 +12,22 @@ def outer_brackets(s):
     for i, c in enumerate(s):
         if c == "(":
             stack.append(i)
-        elif c == ")": 
+        elif c == ")":
             if len(stack) == 1:
-                ret.append(s[stack.pop(): i + 1])
+                ret.append(s[stack.pop():i + 1])
             else:
                 stack.pop()
     return ret
 
-def parse(expr, priority_ops = ['*', '+']):
+
+def evaluate(arg1, op, arg2):
+    if not isinstance(arg1, int): arg1 = int(arg1)
+    if not isinstance(arg2, int): arg2 = int(arg2)
+    method = SYMBOLS[op]
+    return getattr(arg1, method)(arg2)
+
+
+def parse(expr, priority_ops=['*', '+']):
     subexprs = outer_brackets(expr)
     for sbx in subexprs:
         expr = expr.replace(sbx, str(parse(sbx[1:-1], priority_ops)))
@@ -31,31 +37,29 @@ def parse(expr, priority_ops = ['*', '+']):
     stack = []
     for c in tokens:
         if c.isdigit() and len(stack) >= 2 and stack[-1] in priority_ops:
-            op = stack.pop()
-            arg2 = int(stack.pop())
-            new = getattr(int(c), SYMBOLS[op])(arg2)
-            stack.append(new)             
-        else: 
+            new = evaluate(c, stack.pop(), stack.pop())
+            stack.append(new)
+        else:
             stack.append(c)
-    
 
     k = int(stack.pop())
     while len(stack) > 0:
-        op = stack.pop()
-        arg2 = int(stack.pop())
-        k = getattr(k, SYMBOLS[op])(arg2)
+        k = evaluate(k, stack.pop(), stack.pop())
 
     return k
+
 
 def p1():
     t = sum(parse(ln) for ln in N)
     print(t)
     return t
 
+
 def p2():
     t = sum(parse(ln, priority_ops=["+"]) for ln in N)
     print(t)
-    return t              
+    return t
+
 
 p1()
 print('------')
